6b3c42f4f02210dda8c12541b1ac3cd7d0774e9b
SaadElKady <saadelkady45@gmail.com>
2021-09-19 15:40:27 +0200

    first commit

---
+++ FirstProjectV1/Controllers/GamesController.cs
@@ -0,0 +1,45 @@
+﻿using FirstProjectV1.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProjectV1.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var g1 = new Game() { Name = "The Last of us" };
+            var g2 = new Game() { Rating = 10 };
+            return View(g1);
+            return View(g2);
+        }
+
+        public IActionResult Print()
+        {
+            var g1 = new Game() { Name = "The Last of us" };
+            var g2 = new Game() { Rating = 100 };
+            g2.ToString();
+            return View(g1);
+            return View(g2);
+        }
+
+        public IActionResult Signup()
+        {
+            return View();
+        }
+       
+        public IActionResult MainScreen()
+        {
+            return View();
+        }
+
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+
+}
---
+++ FirstProjectV1/Models/Game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstProjectV1.Models
+{
+    public class Game
+    {
+        public String Name{ get; set; }
+        public int Rating{ get; set; }
+    }
+}
